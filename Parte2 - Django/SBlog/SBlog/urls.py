0965c7ef318102ba8f31d11e75452d609accccbe
Vladimir Ilich Ascue Lovon <vladimir.ascue@vlaslov.com>
2023-01-26 14:57:27 -0500

    Primera carga del proyecto SBlog

---
+++ Parte2 - Django/SBlog/SBlog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from Blog.views import crearCategoria, buscarCategoria, actualizarCategoria
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('crearCategoria/', crearCategoria),
+    path('buscarCategoria/', buscarCategoria),
+]
